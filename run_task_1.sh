ef45a6a2058a7d59cbd94aa192899055ec4f4245
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-04-14 00:23:54 +0200

    Update run scripts

--- run_task_1.sh
+++ run_task_1.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# 1. Activate the virtual environment
-source /path/to/your/venv/bin/activate
-
-# 2. Set the PYTHONPATH (this is equivalent to adding extra paths in settings.json)
-export PYTHONPATH=$(pwd)/src:$PYTHONPATH
-
-# 3. Run the specific task's main.py (task_1 in this case)
-python src/task_1/main.py
+# Run Task 2
+python3 src/task_1/main.py
