be3796cb55de3a8dca10c32fceb6b6375698372d
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-04-13 23:00:23 +0200

    Improve log messages with adaptive line length

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -11,6 +11,8 @@ def report_content_errors(errors: list[tuple[int, str, str]], log_file_path: str
     if not errors:
         return
 
+    max_line_length = max((len(line) for _, line, _ in errors), default=0)
+
     message = (
         "There are potentially corrupted data in your dataset file "
         "that may lead to wrong results and should be or may require fix:"
@@ -19,5 +21,8 @@ def report_content_errors(errors: list[tuple[int, str, str]], log_file_path: str
     print_and_log(f"Please check '{log_file_path}' file for more details.", level="INFO", log=False)
 
     for line_idx, line_str, cause in errors:
-        log_message = f"Line {(str(line_idx) + ":").ljust(4)} {line_str.ljust(34)} - Cause: {cause if cause else "Unknown"}"
+        log_message = (
+            f"Line {(str(line_idx) + ':').ljust(4)} "
+            f"{line_str.ljust(max_line_length)} - Cause: {cause if cause else 'Unknown'}"
+        )
         print_and_log(log_message, level="INFO", print_to_console=False)
