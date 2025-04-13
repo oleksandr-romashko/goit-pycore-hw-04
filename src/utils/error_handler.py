5ecc8a6bf410b4a651ee13475cd496910e72218b
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-04-13 21:15:08 +0200

    Update error handler printout

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -13,11 +13,11 @@ def report_content_errors(errors: list[tuple[int, str, str]], log_file_path: str
 
     message = (
         "There are potentially corrupted data in your dataset file "
-        "that may lead to wrong calculation results and should be or may require fix:"
+        "that may lead to wrong results and should be or may require fix:"
     )
     print_and_log(message, level="WARNING")
     print_and_log(f"Please check '{log_file_path}' file for more details.", level="INFO", log=False)
 
     for line_idx, line_str, cause in errors:
-        log_message = f"Line {line_idx}: {line_str.ljust(20)} - Cause: {cause if cause else "Unknown"}"
+        log_message = f"Line {(str(line_idx) + ":").ljust(4)} {line_str.ljust(34)} - Cause: {cause if cause else "Unknown"}"
         print_and_log(log_message, level="INFO", print_to_console=False)
