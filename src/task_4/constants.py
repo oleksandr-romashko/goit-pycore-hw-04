90e2262a2858568f87f5b2cdb669ae3e67f85429
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-04-15 11:52:18 +0200

    Fix constants phone help message

--- src/task_4/constants.py
+++ src/task_4/constants.py
@@ -1,2 +1,2 @@
 HELP_TIP_STR = "type 'help' for the available list of commands"
-PHONE_FORMAT_DESC_STR = "9–15 digits, may include '+' at the beginning"
+PHONE_FORMAT_DESC_STR = "9-15 digits, may include '+' at the beginning"
